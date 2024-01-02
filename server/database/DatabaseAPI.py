b2a1e9c18a0c166deb518b7c10c0c05be4c32aa4
Rhythm Deolus <122043902+RhythmDeolus@users.noreply.github.com>
2024-01-02 22:53:42 +0530

    moved from flask to fastapi (#2)

--- server/database/DatabaseAPI.py
+++ server/database/DatabaseAPI.py
@@ -1,5 +1,7 @@
 from pgvector.psycopg2 import register_vector
 import face_recognition as fr
+import numpy as np
+from datetime import datetime
 
 
 class DatabaseAPI:
@@ -7,16 +9,18 @@ class DatabaseAPI:
         self.conn = conn
         register_vector(conn)
 
-    def getStudentID(self, embedding):
+    def getStudentID(self, embedding: list[float]) -> int | None:
         cursor = self.conn.cursor()
-        cursor.execute('SELECT student_id FROM face_id ORDER BY face_embedding <=> %s LIMIT 5;', (embedding, ))
+        embedding = np.array(embedding)
+        cursor.execute('SELECT student_id, face_embedding FROM face_id ORDER BY face_embedding <=> %s LIMIT 5;', (embedding, ))
         for record in cursor:
-            if fr.compare_faces([record[0]], embedding, tolerance=0.6)[0]:
+            if fr.compare_faces([record[1]], embedding, tolerance=0.6)[0]:
                 return record[0]
         return None
 
-    def registerStudent(self, embedding, studentID):
+    def registerStudent(self, embedding: list[float], studentID: int) -> bool:
         cursor = self.conn.cursor()
+        embedding = np.array(embedding)
         try:
             cursor.execute('INSERT INTO face_id (student_id, face_embedding) VALUES %s;', ((studentID, embedding),))
             self.conn.commit()
@@ -25,7 +29,7 @@ class DatabaseAPI:
             print(e)
             return False
 
-    def deleteStudent(self, studentID):
+    def deleteStudent(self, studentID: int) -> bool:
         cursor = self.conn.cursor()
         try:
             cursor.execute('DELETE FROM face_id WHERE student_id = %s;', (studentID,))
@@ -34,11 +38,17 @@ class DatabaseAPI:
         except Exception:
             return False
 
-    def update_embedding(self, student_id, embedding):
+    def update_embedding(self, student_id: int, embedding: list[float]) -> bool:
         cursor = self.conn.cursor()
+        embedding = np.array(embedding)
         try:
             cursor.execute('UPDATE face_id SET face_embedding = %s WHERE student_id = %s;', (embedding, student_id,))
             self.conn.commit()
             return True
         except Exception:
             return False
+
+    def mark_attendance(self, student_id: int, time: datetime) -> bool:
+        print('marked attendance for: ', student_id)
+        print('at time: ', time)
+        return True
