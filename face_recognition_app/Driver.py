27d5b66cc5347ea9e4679e54f0181e9abe097631
RhythmDeolus <122043902+RhythmDeolus@users.noreply.github.com>
2024-01-03 19:41:22 +0530

    made face_recognition_app to be multiprocessed

--- face_recognition_app/Driver.py
+++ face_recognition_app/Driver.py
@@ -3,8 +3,7 @@ from Display import Display
 from FaceRecog import FaceRecog
 import face_recognition
 
-import threading
-import queue
+import multiprocessing
 
 import tracemalloc
 import time
@@ -12,20 +11,8 @@ import datetime
 
 import requests
 
-tracemalloc.start()
-
-c = Camera()
 
 f = FaceRecog()
-
-c.open()
-
-d = Display(0, 'Main Window')
-
-i = 0
-n = 10
-
-q = queue.Queue()
 
 cache = {
   'index': [],
@@ -43,44 +30,62 @@ def mark_attendance(url, json):
     return {}
 
 
-def worker():
+def worker(q):
     url = 'http://127.0.0.1:8000/api_1/mark_attendance'
     while True:
         image = q.get()
+
         if image is None:
             print("Couldn't catch Image")
-            q.task_done()
             continue
         else:
             frame = f.resizeImg(image, 0.25)
         faces = f.detectFaces(frame)
         encodings = f.getEncodings(frame, faces)
         for encoding in encodings:
-            matches = face_recognition.compare_faces(cache['index'],
-                                                     encoding, tolerance=0.6)
             try:
+                matches = face_recognition.compare_faces(cache['index'],
+                                                         encoding,
+                                                         tolerance=0.6)
                 idx = matches.index(True)
                 print('cache hit')
                 print('student id: ', cache['value'][idx])
             except Exception:
                 res = mark_attendance(url,
                                       {'embedding': encoding.tolist(),
-                                       'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
-                if ('id' in res):
+                                       'time': datetime.datetime.now()
+                                       .strftime("%Y-%m-%d %H:%M:%S")})
+                if ('id' in res) and res['id'] is not None:
                     print('Attendance was marked for student id: ', res['id'])
                     cache['index'].append(res['embedding'])
                     cache['value'].append(res['id'])
-        q.task_done()
+                else:
+                    print("couldn't find any record of you")
 
 
-threading.Thread(target=worker, daemon=True).start()
+if __name__ == "__main__":
+    tracemalloc.start()
 
-while True:
-    time.sleep(0.16)
-    frame1 = c.get_image()
-    if i % n == 0:
-        i = 0
-        if q.empty():
-            q.put(frame1, False)
-    d.displayimg(frame1)
-    i += 1
+    c = Camera()
+
+    c.open()
+
+    d = Display(0, 'Main Window')
+
+    i = 0
+    n = 10
+
+    q = multiprocessing.Queue()
+
+    p = multiprocessing.Process(target=worker, daemon=True, args=(q, ))
+    p.start()
+
+    while True:
+        # time.sleep(0.16)
+        frame1 = c.get_image()
+        if i % n == 0:
+            i = 0
+            if q.empty():
+                q.put(frame1, False)
+        d.displayimg(frame1)
+        i += 1
